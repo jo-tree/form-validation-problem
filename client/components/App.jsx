8e18e13e14a8c0ccb9c86a8b52ac1b094e06bd26
Jo Tregidga <84287388+jo-tree@users.noreply.github.com>
2021-10-14 13:45:45 +1300

    form styled and functioning to hold state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,41 +1,93 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 
 function App () {
+  
+  const [userData, setNewUser] = useState ({
+    email: '',
+    password: '',
+  })
 
-  const Checkbox = ({ label, value, onChange }) => {
-    return (
-      <label>
-        <input type='checkbox' checked={value} onChange={onChange} />
-        {label}
-      </label>
-    )
-  }  
+  const [newColour, setColour] = useState ('blue')
 
 
+  // const [checked, setChecked] = useState (false)
+  const [newAnimals, setAnimals] = useState ({
+    bear: false,
+    tiger: false,
+    snake: false,
+    donkey: false
+  })
+
+
+  const [tigerType, setType] = useState ('')
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    setNewUser({
+      ...userData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleColour = (e) => {
+    e.preventDefault()
+    setColour(e.target.value)
+  }
+
+  const handleAnimals = (e) => {
+    setAnimals({
+      ...newAnimals,
+      [e.target.name]: e.target.checked
+    })
+  }
+
+  const handleType = (e) => {
+    e.preventDefault()
+    setType(e.target.value)
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert('Thank you! Your information has been sent off into the void')
+
+  }
+
+
+
+  const { email, password } = userData
+  const { bear, tiger, snake, donkey } = newAnimals
 
   return (
     <>
       <div className='form'>
       <h2>Contact form</h2>
       <form>
+      <div className='eachLine'>
         <label>Email:</label>
         <input 
           type='email'
+          placeholder='Email'
           id='email'
           name='email'
-          //value='{handleChange}'
+          value={email}
+          onChange={handleChange}
         />
         <label>Password:</label>
         <input 
           type="password"
+          placeholder='minimum 8 characters'
           pattern=".{8,}"
           id='password'
           name='password'
-          //value='{handleChange}'
+          value={password}
+          onChange={handleChange}
         />
+        </div>
+        <div className='eachLine'>
         <label>Colour:
-          <select name='colour'>
+          <select name='colour' onChange={handleColour}>
             <option value='blue'>Blue</option>
             <option value='green'>Green</option>
             <option value='red'>Red</option>
@@ -43,35 +95,68 @@ function App () {
             <option value='brown'>Brown</option>
           </select>
         </label>
-        <div>
-          <Checkbox 
-            label='Bear'
-            // value={checked}
-            // onChange={onChange}
+        </div>
+        <div className='eachLine'>
+          <label> Pick your favourite animals:</label>
+        </div>
+        <div className='animalCheckbox'>
+          <input
+          type='checkbox' 
+          label='Bear'
+          name='bear'
+          checked={bear}  
+          onChange={handleAnimals}
+          />
+          Bear
+        </div>
+        <div className='animalCheckbox'>
+            <input
+            type='checkbox' 
+            name='tiger'
+            checked={tiger}
+            onChange={handleAnimals}
             />
-            <Checkbox 
-            label='Tiger'
-            // value={checked}
-            // onChange={onChange}
-            />
-            <label>Type of tiger:</label>
+            Tiger
+            <div>
+            {(newAnimals.tiger === false) ? null :
+          <>
             <input 
               type='tigerType'
+              placeholder='Type of tiger'
+              autoComplete='off'
               id='tigerType'
               name='tigerType'
-              //value='{handleChange}'
+              value={tigerType}
+              onChange={handleType}
+              required
             />
-
-            <Checkbox 
+          </>}
+          </div>
+        </div>
+        <div className='animalCheckbox'>
+            <input
+            type='checkbox' 
             label='Snake'
-            // value={checked}
-            // onChange={onChange}
+            name='snake'
+            checked={snake}
+            onChange={handleAnimals}
             />
-            <Checkbox 
+            Snake
+        </div>
+        <div className='animalCheckbox'>
+            <input
+            type='checkbox' 
             label='Donkey'
-            // value={checked}
-            // onChange={onChange}
+            name='donkey'
+            checked={donkey}
+            onChange={handleAnimals}
             />
+            Donkey
+        </div>
+          <div className='eachLine'>
+          <button onSubmit={handleSubmit}>
+            Send off into the void
+          </button>
         </div>
       </form>
       </div>
